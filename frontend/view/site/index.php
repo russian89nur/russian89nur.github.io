98209c4b1233357fa7e4a53f93a2df2a333283ee
Станислав <dobro_333@mail.ru>
2018-06-25 00:10:07 +0500

    burger lading (pc)

---
+++ frontend/view/site/index.php
@@ -0,0 +1,78 @@
+<?php include '../main/header.php';?>
+    <img src="../../../common/img/background/1.jpg" class="position-fixed w-100 h-vh-all z-ind-01" alt="">
+    <div class="container">
+        <div class="col-12 d-flex flex-column h-vh-all align-items-center">
+            <span class="mt-auto f-s-caption font-weight-bold">Зарабатывайте от 150 000 ₽ ежемесячно через 1,5 месяца за счёт открытия fastfood-бургерной</span>
+            <div class="mb-auto f-s-30 font-weight-bold">
+                <span>по франшизе </span><span class="c-red">Burger House</span>
+            </div>
+        </div>
+        <div class="col-12 px-3 h-vh-all position-static d-flex flex-column">
+            <div class="substrate-white bgr-c-white position-absolute w-100 h-vh-all left-0 z-ind-01"></div>
+            <div class="row pt-5">
+                <div class="col-3 d-flex flex-column">
+                    <span class="c-black border-bottom-3 pb-3 border-danger font-weight-bold f-s-26">Инвестиции</span>
+                    <span class="c-black pt-3">от 250 000₽ — мини-бургерная</span>
+                    <span class="c-black">от 450 000₽ — кафе или фудкорт</span>
+                </div>
+                <div class="col-3 d-flex flex-column">
+                    <span class="c-black border-bottom-3 pb-3 border-danger font-weight-bold f-s-26">Паушальный взнос</span>
+                    <span class="c-black pt-3">от 200 000₽</span>
+                </div>
+                <div class="col-3 d-flex flex-column">
+                    <span class="c-black border-bottom-3 pb-3 border-danger font-weight-bold f-s-26">Роялти</span>
+                    <span class="c-black pt-3">3% с 2-го месяца</span>
+                </div>
+                <div class="col-3 d-flex flex-column">
+                    <span class="c-black border-bottom-3 pb-3 border-danger font-weight-bold f-s-26">Окупаемость</span>
+                    <span class="c-black pt-3">8 месяцев</span>
+                </div>
+            </div>
+            <span class="c-red font-weight-bold f-c-42 mx-auto pt-5">ШЕСТЬ ПРИЧИН</span>
+            <span class="font-weight-bold f-c-42 c-black mx-auto">стать нашим партнёром</span>
+            <div class="row pt-5">
+                <div class="col-4 d-flex text-center flex-column">
+                    <span class="c-black pb-3 font-weight-bold f-s-26">Выгодная ниша</span>
+                    <span class="c-black pt-3">Высокая маржа, низкие издержки, программы лояльности.</span>
+                </div>
+                <div class="col-4 d-flex text-center flex-column">
+                    <span class="c-black pb-3 font-weight-bold f-s-26">Низкий объём инвестиций</span>
+                    <span class="c-black pt-3">Небольшие площади торговых точек — 30-50 кв.м.</span>
+                </div>
+                <div class="col-4 d-flex text-center flex-column">
+                    <span class="c-black pb-3 font-weight-bold f-s-26">Устойчивый рынок</span>
+                    <span class="c-black pt-3">Каждый 5-й россиянин питается фастфудом, и это не зависит от кризиса.</span>
+                </div>
+                <div class="col-4 d-flex text-center flex-column pt-5">
+                    <span class="c-black pb-3 font-weight-bold f-s-26">4-летний опыт работы</span>
+                    <span class="c-black pt-3">Занимаемся ресторанным бизнесом с 2013 года</span>
+                </div>
+                <div class="col-4 d-flex text-center flex-column pt-5">
+                    <span class="c-black pb-3 font-weight-bold f-s-26">3 пакета франшизы</span>
+                    <span class="c-black pt-3">Сможете начать с малого, а далее расшириться до сети</span>
+                </div>
+                <div class="col-4 d-flex text-center flex-column pt-5">
+                    <span class="c-black pb-3 font-weight-bold f-s-26">Соблюдаем закон</span>
+                    <span class="c-black pt-3">Используем онлайн-кассу согласно ФЗ №54; работаем с системой ЕГАИС</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 px-3 h-vh-all position-static d-flex flex-column">
+            <div class="substrate-white bgr-c-white position-absolute w-100 h-vh-all left-0 z-ind-01"></div>
+            <div class="row my-auto">
+                <span class="c-black mx-auto">Слайдер</span>
+            </div>
+        </div>
+        <div class="col-12 px-3 h-vh-all position-static d-flex flex-column">
+            <img src="../../../common/img/background/2.jpg" class="substrate-white bgr-c-white position-absolute w-100 h-100 left-0 z-ind-01">
+            <span class="pt-5 mt-5 f-s-caption font-weight-bold text-center">Оставьте свои контакты и узнайте
+о преимуществах заведений каждого типа!</span>
+        </div>
+        <div class="col-12 px-3 h-vh-all position-static d-flex flex-column">
+                <div class="substrate-white bgr-c-white position-absolute w-100 h-vh-all left-0 z-ind-01"></div>
+            <div class="row my-auto">
+                <span class="c-black mx-auto">Слайдер</span>
+            </div>
+        </div>
+    </div>
+<?php include '../main/footer.php'; ?>
