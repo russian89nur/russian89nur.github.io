b8abce98a9e6c2204cd69edfe64d69561b1bdb2b
Станислав <dobro_333@mail.ru>
2018-07-18 01:01:05 +0500

    1

--- frontend/view/site/index.php
+++ frontend/view/site/index.php
@@ -1,16 +1,46 @@
-<?php include '../main/header.php';
-    // mail("dobro_333@mail.ru", "Тест", "Line 1\n Line 2\n Line 3");
-    // $to = "dobro_333@mail.ru";
-    // $subjaact = "Тест";
-    // $mes ="Line 1\n Line 2\n Line 3";
-
-    // // $header = "Content-type:text/html; charset = windows-1251 \r\n";
-    // $header = "From: dobro_333@mail.ru";
-    // // $header .= "Reply to dobro_333@mail.ru";
-
-    //  mail($to, $subjaact, $mes, $header);
-
-?>
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Главная</title>
+        <link rel="stylesheet" href="../../../web/css/bootstrap/bootstrap.css">
+        <link rel="stylesheet" href="../../web/css/site.css">
+    </head>
+    <body>
+<nav class="navbar navbar-expand-lg navbar-light position-absolute w-100 z-ind-1">
+<a class="navbar-brand ml-auto" href="#">Бургер</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Выгоды <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Форматы заведений</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Меню</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Пакеты франшиз</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Инвестиции</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Что входит</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Контакты</a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <a class="nav-link disabled" href="#">8 (912) 041 13 12</a>
+        </form>
+    </div>
+</nav>
 <img src="../../../common/img/background/1.jpg" class="position-fixed w-100 h-vh-all z-ind-01 img_back" alt="">
 <div class="container">
         <div class="col-12 d-flex flex-column h-vh-all align-items-center">
@@ -723,4 +753,8 @@
         </div>
     </div>
 </div>
-<?php include '../main/footer.php'; ?>
+<script type="text/javascript" src="../../../web/js/jquery/jquery-3.3.1.js"></script>
+        <script type="text/javascript" src="../../../web/js/bootstrap/bootstrap.js"></script>
+        <script type="text/javascript" src="../../web/js/script.js"></script>
+    </body>
+</html>
