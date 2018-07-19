36505e50b812d0a34266212a022788dd569cf00b
Станислав <dobro_333@mail.ru>
2018-07-20 01:28:55 +0500

    version 4.0

--- frontend/view/main/header.php
+++ frontend/view/main/header.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
-<html lang="ru" dir="ltr">
+<html lang="ru" >
     <head>
-        <meta charset="utf-8">
         <title>Главная</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="../../../web/css/bootstrap/bootstrap.css">
         <link rel="stylesheet" href="../../web/css/site.css">
     </head>
